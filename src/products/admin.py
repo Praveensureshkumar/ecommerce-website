2d923b49140f3744f91778146a1fa97810f0d512
Praveensureshkumar <praveen0suresh@gmail.com>
2025-03-26 14:18:38 +0530

    modified index file

--- src/products/admin.py
+++ src/products/admin.py
@@ -3,3 +3,4 @@ from django.contrib import admin
 # Register your models here.
 from products.models import *
 admin.site.register(index_product_details)
+admin.site.register(product_category)
--- src/products/models.py
+++ src/products/models.py
@@ -1,7 +1,10 @@
 from django.db import models
 
-class index_product_details(models.Model):
+class product_category(models.Model):
+    category=models.CharField(max_length=200,primary_key=True)
 
+class index_product_details(models.Model):
+    product_categorys=models.ForeignKey(product_category,on_delete=models.CASCADE,null=True,blank=True)
     product_id = models.AutoField(primary_key=True)
     product_name = models.CharField(max_length=30)
     product_description = models.CharField(max_length=100, null=True, blank=True)
